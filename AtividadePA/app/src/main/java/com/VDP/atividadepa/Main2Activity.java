87280c96e6d84689ce24dc37e127eaefb4b51817
Vinicius <vinicius.d.paranhos@gmail.com>
2019-09-02 22:22:23 -0300

    atividade 03/09

---
+++ AtividadePA/app/src/main/java/com/VDP/atividadepa/Main2Activity.java
@@ -0,0 +1,40 @@
+package com.VDP.atividadepa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView txt1;
+    TextView txt2;
+    Button butao2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        txt1 = findViewById(R.id.txt1);
+        txt2 = findViewById(R.id.txt2);
+        butao2 = findViewById(R.id.butao2);
+
+        Intent i = getIntent();
+        Produto produto = i.getParcelableExtra("produto");
+
+        txt1.setText(produto.getDescricao());
+        String precoTotal= ""+ (produto.getQuantidade() * produto.getValorUni());
+        txt2.setText(precoTotal);
+        butao2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
