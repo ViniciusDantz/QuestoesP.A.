b5d6fcb71a97b341379de432230f6e6e41b16c8f
Vinicius <vinicius.d.paranhos@gmail.com>
2019-09-16 23:23:21 -0300

    atividade LogCat feita

---
+++ AppLogCat/app/src/main/java/mylockscreen/vdp/applogcat/MainActivity.java
@@ -0,0 +1,14 @@
+package mylockscreen.vdp.applogcat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
